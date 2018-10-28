725ddf76b4201531d6f5e1ea9b6a0e716834dcd9
Bruno Paulino <brunojppb@gmail.com>
2018-10-28 14:38:06 +0100

    Initial Commit

---
+++ sidemenutest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sidemenutest
+//
+//  Created by Bruno Paulino on 28.10.18.
+//  Copyright © 2018 bpaulino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
